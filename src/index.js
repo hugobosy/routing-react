156dc07f91c6ed0bfae0f14d289ceac6701a68ac
HugoBosy <hkliszcz@gmail.com>
2022-05-31 17:22:23 +0200

    add structure spa

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './layouts/App';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,12 +1,25 @@
 import '../styles/App.css';
 import {BrowserRouter as Router} from "react-router-dom";
+import {Header} from "./Header";
+import {Aside} from "./Aside";
+import {Page} from "./Page";
+import {Footer} from "./Footer";
 
 
 function App() {
     return (
         <Router>
             <div className="App">
-                SPA
+                <header>
+                    {<Header/>}
+                </header>
+                <main>
+                    {<Aside/>}
+                    {<Page/>}
+                </main>
+                <footer>
+                    {<Footer/>}
+                </footer>
             </div>
         </Router>
     );
