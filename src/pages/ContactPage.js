e84494f2dda5072d7ceac6e5c6e544c4789c1dd0
HugoBosy <hkliszcz@gmail.com>
2022-06-01 14:27:46 +0200

    edit contact page & add styles

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import '../styles/ContactPage.css';
 
 export const ContactPage = () => {
+
+    const [text, setText] = useState('');
+
+    const handleValue = (e) => {
+        setText(e.target.value);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setText('');
+    }
     return (
-        <div>Kontakt</div>
+        <div className="contact">
+            <form onSubmit={handleSubmit}>
+                <h1>Napisz do nas!</h1>
+                <textarea value={text} onChange={handleValue} placeholder="Wpisz tekst..."></textarea>
+                <button>Wyślij</button>
+            </form>
+        </div>
     )
 }
