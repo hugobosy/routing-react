4c180d411a253bec25d0e6d17ec6baf0f92640f2
HugoBosy <hkliszcz@gmail.com>
2022-06-01 12:02:21 +0200

    add pages & routing

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -1,7 +1,20 @@
 import React from "react";
+// import {Route} from "react-router-dom";
+import {HomePage} from "../pages/HomePage";
+import {ProductPage} from "../pages/ProductPage";
+import {ContactPage} from "../pages/ContactPage";
+import {AdminPage} from "../pages/AdminPage";
+import {ErrorPage} from "../pages/ErrorPage";
+import {Routes, Route} from "react-router";
 
 export const Page = () => {
     return (
-        <div>Page</div>
+        <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/product" element={<ProductPage/>}/>
+                <Route path="/contact" element={<ContactPage/>}/>
+                <Route path="/admin" element={<AdminPage/>}/>
+                <Route element={<ErrorPage/>}/>
+        </Routes>
     )
 }
