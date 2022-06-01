3451f8bfa0ac8918d5a7b180ce00ba1d2fe145da
HugoBosy <hkliszcz@gmail.com>
2022-06-01 11:18:43 +0200

    add HeaderImage component & image & edit styles

---
+++ src/components/HeaderImage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import '../styles/HeaderImage.css';
+
+export const HeaderImage = props => {
+    return (
+        <img src={props.image} alt="Fish"/>
+    )
+}
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,7 +1,21 @@
 import React from "react";
+import {Route, Routes} from "react-router-dom";
+import {HeaderImage} from "../components/HeaderImage";
+
+import img1 from '../images/carp1.jpg';
+import img2 from '../images/carp2.jpg';
+import img3 from '../images/carp3.jpg';
+import img4 from '../images/carp4.jpg';
 
 export const Header = () => {
     return (
-        <div>Header</div>
+        <Routes>
+
+            <Route path="/" element={<HeaderImage image={img1}/>}/>
+            <Route path="/product" element={<HeaderImage image={img2}/>}/>
+            <Route path="/contact" element={<HeaderImage image={img3}/>}/>
+            <Route path="/admin" element={<HeaderImage image={img4}/>}/>
+
+        </Routes>
     )
 }
