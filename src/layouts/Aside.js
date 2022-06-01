aa967b4e80797459a6ffd9300f6afdf977efdf66
HugoBosy <hkliszcz@gmail.com>
2022-06-01 09:47:17 +0200

    add styles aside & menu

--- src/layouts/Aside.js
+++ src/layouts/Aside.js
@@ -1,7 +1,29 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
+import '../styles/Aside.css';
+
+const list = [
+    {name: "start", path: "/"},
+    {name: "produkty", path: "/product"},
+    {name: "kontakt", path: "/contact"},
+    {name: "panel admina", path: "/admin"},
+]
 
 export const Aside = () => {
+
+    const menu = list.map(item => (
+        <li key={item.name}>
+            <NavLink to={item.path}>{item.name}</NavLink>
+        </li>
+    ))
+
     return (
-        <aside>Aside</aside>
+        <aside>
+            <nav className="main-nav">
+                <ul>
+                    {menu}
+                </ul>
+            </nav>
+        </aside>
     )
 }
